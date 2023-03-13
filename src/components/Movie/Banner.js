96216d68b8f32ccdcfc1220ae7aa378c5febcfb2
Bipin Dahal <bpn11dahal@gmail.com>
2023-03-13 17:14:05 +0545

    navbar and footer added - fixed issues in next and previous buttons - added Router to navigate home page as default

--- src/components/Movie/Banner.js
+++ src/components/Movie/Banner.js
@@ -2,22 +2,27 @@ import { useState } from "react";
 import "assets/scss/index.scss";
 import Details from "./Details";
 import Sliders from "./Sliders";
+import Buttons from "./Buttons";
 
 export default function Banner() {
   const [bannerData, setBannerData] = useState();
+  const [scrollLeft, setScrollLeft] = useState(0);
 
   const handleClick = (movie) => {
     setBannerData(movie);
   };
 
   return (
-    <div className="position-relative">
-      <div className="m-2 d-flex flex-column align-items-center position-relative banner-container">
-        <div className="position-relative m-2 d-flex w-100 flex-column-reverse flex-sm-row movie-banner">
-          <Details movie={bannerData} />
-        </div>
-        <Sliders handleClick={handleClick} />
+    <div className="m-2 d-flex flex-column align-items-center position-relative banner-container">
+      <div className="position-relative m-2 d-flex w-100 flex-column-reverse flex-sm-row movie-banner">
+        <Details movie={bannerData} />
       </div>
+      <Sliders
+        handleClick={handleClick}
+        scrollLeft={scrollLeft}
+        setScrollLeft={setScrollLeft}
+      />
+      <Buttons scrollLeft={scrollLeft} setScrollLeft={setScrollLeft} />
     </div>
   );
 }
